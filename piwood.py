8fb3f046c8d540b0cb4d24ad9be4b227165bb009
Sam Graham <samgraham@outlook.com>
2016-06-29 00:23:03 -0600

    Implemented better table to database integrations

--- piwood.py
+++ piwood.py
@@ -2,13 +2,15 @@
 
 import sys
 import time
+from sqlQueries import dropQuery, createQuery, insertEmptyRacerQuery
 from PySide import QtCore
 from PySide.QtSql import *
 from PySide.QtGui import *
 from PySide.QtDeclarative import QDeclarativeView
 
 def stubFunction():
-    print("Hello")
+    print("Stub Function Executed")
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -16,31 +18,59 @@ class MainWindow(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        # TODO: https://pyside.github.io/docs/pyside/PySide/QtSql/QSqlTableModel.html#PySide.QtSql.QSqlTableModel
-        db = QSqlDatabase.addDatabase("QSQLITE")
-        db.setDatabaseName("piwood.db")
-        db.open()
+        self.db = QSqlDatabase.addDatabase("QSQLITE")
+        self.db.setDatabaseName("piwood.db")
+        self.db.open()
 
-        model = QSqlTableModel(None, db)
-        model.setTable("racers")
-        model.setEditStrategy(QSqlTableModel.OnFieldChange)
+        # Show if there is a problem with the database connection
+        print("Database Connection", self.db.isValid())
+
+        self.model = QSqlTableModel(None, self.db)
+        self.model.setTable("racers")
+        self.model.setEditStrategy(QSqlTableModel.EditStrategy.OnFieldChange)
+
+        # Populates the table, needed to show data
+        self.model.select()
 
         projectView = QTableView()
-        projectView.setModel(model)
+        projectView.setModel(self.model)
 
         # TODO: Throw into seperate loop and test
+        controls = QWidget()
+        controlsLayout = QVBoxLayout()
+
         testButton = QPushButton("Test Sensors and Displays")
         testButton.setToolTip("Run a test on the track's sensors and displays")
         testButton.clicked.connect(stubFunction)
 
+        insertButton = QPushButton("Insert Racer")
+        insertButton.setToolTip("Adds racer to table")
+        insertButton.clicked.connect(self.insertRacer)
+
+        saveButton = QPushButton("Save the table")
+        saveButton.setToolTip("Save the table to the database.")
+        saveButton.clicked.connect(self.saveTable)
+
+        clearButton = QPushButton("Clear the table")
+        clearButton.setToolTip("Clear the table")
+        clearButton.clicked.connect(stubFunction)
+
+        controlsLayout.addWidget(testButton)
+        controlsLayout.addWidget(insertButton)
+        controlsLayout.addWidget(saveButton)
+        controlsLayout.addWidget(saveButton)
+
+        controls.setLayout(controlsLayout)
+
         rightDockWidget = QDockWidget()
-        rightDockWidget.setWidget(testButton)
+        rightDockWidget.setWidget(controls)
         rightDockWidget.setFeatures(QDockWidget.NoDockWidgetFeatures)
 
         self.setCentralWidget(projectView)
         self.addDockWidget(QtCore.Qt.DockWidgetArea.RightDockWidgetArea,
                            rightDockWidget)
 
+        # &E allows users to use Alt E
         exitAction = QAction('&Exit', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit application')
@@ -53,10 +83,33 @@ class MainWindow(QMainWindow):
 
         self.statusBar().showMessage('Ready')
 
+        # In case the windows is shrunk
         self.setGeometry(300,300,250,150)
         self.showMaximized()
         self.setWindowTitle('PiWood-Derby')
         self.show()
+
+    def insertRacer(self):
+        # -1 Puts the record at the end of the database
+        if QSqlQuery(insertEmptyRacerQuery, self.db) == False:
+            print(self.model.lastError())
+        # Populates the table, needed to show data
+        self.model.select()
+
+
+    # Save all changes to the table
+    def saveTable(self):
+        if self.model.submitAll() == False:
+            print(self.model.lastError())
+
+    def clearTable(self):
+        QSqlQuery(dropQuery, self.db)
+        QSqlQuery(createQuery, self.db)
+
+    def close(self):
+        self.db.close()
+        print("DB connection closed")
+        super(MainWindow, self).close()
 
 class PiWood:
     def __init__(self):
--- sqlQueries.py
+++ sqlQueries.py
@@ -1,2 +1,6 @@
-dropQuery = "DROP TABLE IF EXISTS racers"
-createQuery = "CREATE TABLE IF NOT EXISTS racers (id INTEGER PRIMARY KEY, name TEXT, time_1 INTEGER, time_2 INTEGER, time_3 INTEGER, average INTEGER, scale_speed INTEGER)"
+# This file contains all the needed SQL queries that run the PiWood Derby
+
+dropQuery = "DROP TABLE IF EXISTS racers;"
+createQuery = "CREATE TABLE IF NOT EXISTS racers (id INTEGER PRIMARY KEY, name TEXT, time_1 INTEGER, time_2 INTEGER, time_3 INTEGER, average INTEGER, scale_speed INTEGER);"
+insertEmptyRacerQuery = "INSERT INTO racers (name, time_1, time_2, time_3, average, scale_speed) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);"
+
