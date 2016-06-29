217e9af38c31efd0db1c487cd471a1700f2d3130
Sam Graham <samgraham@outlook.com>
2016-06-28 20:25:17 -0600

    Abstracted out database queries

--- installDatabase.py
+++ installDatabase.py
@@ -2,6 +2,7 @@
 
 import sys
 import time
+from sqlQueries import dropQuery, createQuery
 from PySide import QtCore
 from PySide.QtSql import *
 
@@ -9,7 +10,9 @@ db = QSqlDatabase.addDatabase("QSQLITE")
 db.setDatabaseName("piwood.db")
 db.open()
 
-QSqlQuery("DROP TABLE IF EXISTS racers", db)
-QSqlQuery("CREATE TABLE IF NOT EXISTS racers (id INTEGER PRIMARY KEY, name TEXT, time_1 INTEGER, time_2 INTEGER, time_3 INTEGER, average INTEGER, scale_speed INTEGER)", db)
+QSqlQuery(dropQuery, db)
+QSqlQuery(createQuery, db)
+
+db.close()
 
 
---
+++ sqlQueries.py
@@ -0,0 +1,2 @@
+dropQuery = "DROP TABLE IF EXISTS racers"
+createQuery = "CREATE TABLE IF NOT EXISTS racers (id INTEGER PRIMARY KEY, name TEXT, time_1 INTEGER, time_2 INTEGER, time_3 INTEGER, average INTEGER, scale_speed INTEGER)"
