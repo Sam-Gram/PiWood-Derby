827da0ec0f91f6503ff55a97e5ef7ae6fc92dfd7
Sam Graham <samgraham@outlook.com>
2016-07-03 14:55:23 -0600

    Added displays class

---
+++ displays.py
@@ -0,0 +1,25 @@
+from Adafruit_LED_Backpack import SevenSegment
+
+class Displays():
+    def __init__(self):
+        # Change busnum to 0 if using a pi older than B+
+        self.blueDisplay   = SevenSegment.SevenSegment(address=0x70, busnum = 1)
+        self.redDisplay    = SevenSegment.SevenSegment(address=0x71, busnum = 1)
+        self.greenDisplay  = SevenSegment.SevenSegment(address=0x72, busnum = 1)
+
+        self.displayList = [self.blueDisplay, self.redDisplay, self.greenDisplay]
+
+        # Needed to allow displays to work
+        for display in self.displayList:
+            display.begin()
+
+    # Takes a set of 3 numbers and displays them in order: blue, red and green
+    def displayTimes(timesList):
+        for i, display in enumerate(displayList):
+            display.clear()
+
+            # Ensure that the the number fits on the displays
+            # Adjust the digit place depending if the time is ten seconds or greater
+
+            display.write_float(timesList[i] % 100, decimal_digits=(3 if timesList[i] < 10 else 2))
+            display.write_display()
